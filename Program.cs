897cfb3a81ee93c456d4be1a6396d43649df938b
Erik Sabino <eriksabino@Eriks-MacBook-Air.local>
2025-03-29 14:00:30 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿Console.WriteLine("Tente adivinhar a palavra secreta, ela tem 6 letras");
+Console.WriteLine("Você tem 6 tentativas");
+Console.WriteLine("A palavra secreta é: _ _ _ _ _ _");
+Console.WriteLine("Digite uma letra:");
+string palavra = "banana";
+
+string letra = Console.ReadLine()!;
+if (palavra.Contains(letra))
+{
+    Console.WriteLine($"A palavra secreta contém a letra: '{letra}'!");
+}
+else
+{
+    Console.WriteLine($"A palavra secreta não contém a letra '{letra}'.");
+}
+
+letra = Console.ReadLine()!;
+if (palavra.Contains(letra))
+{
+    Console.WriteLine($"A palavra secreta contém a letra: '{letra}'!");
+}
+else
+{
+    Console.WriteLine($"A palavra secreta não contém a letra '{letra}'.");
+}
+
+letra = Console.ReadLine()!;
+if (palavra.Contains(letra))
+{
+    Console.WriteLine($"A palavra secreta contém a letra: '{letra}'!");
+}
+else
+{
+    Console.WriteLine($"A palavra secreta não contém a letra '{letra}'.");
+}
+
+letra = Console.ReadLine()!;
+if (palavra.Contains(letra))
+{
+    Console.WriteLine($"A palavra secreta contém a letra: '{letra}'!");
+}
+else
+{
+    Console.WriteLine($"A palavra secreta não contém a letra '{letra}'.");
+}
+
+letra = Console.ReadLine()!;
+if (palavra.Contains(letra))
+{
+    Console.WriteLine($"A palavra secreta contém a letra: '{letra}'!");
+}
+else
+{
+    Console.WriteLine($"A palavra secreta não contém a letra '{letra}'.");
+}
+
+letra = Console.ReadLine()!;
+if (palavra.Contains(letra))
+{
+    Console.WriteLine($"A palavra secreta contém a letra: '{letra}'!");
+}
+else
+{
+    Console.WriteLine($"A palavra secreta não contém a letra '{letra}'.");
+}
+
+Console.WriteLine("Digite a palavra secreta:");
+palavra = Console.ReadLine()!;
+if (palavra == "banana")
+{
+    Console.WriteLine("Você acertou a palavra secreta");
+}
+else
+{
+    Console.WriteLine("Você não acertou a palavra secreta");
+}
+Console.WriteLine("a palavra secreta é: " + palavra);
+
+
+
+
+
+    
+
+
+
+    
+
+
